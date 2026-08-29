92753d2f96496a60c997b97f1efc6bfd9ae62c13
ishapitale16 <isha.pitale@mitwpu.edu.in>
2026-08-29 10:24:45 +0530

    Add DivideByZero class with exception handling

---
+++ DivideByZero.java
@@ -0,0 +1,16 @@
+public class DivideByZero {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+        finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
